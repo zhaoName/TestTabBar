04baf49b7407ab155e8e45556c63c12043814ba1
zhaoName <zhao1529835@126.com>
2016-06-15 11:56:15 +0800

    cocoa pods

--- Podfile
+++ Podfile
@@ -1,2 +1,6 @@
-platform:ios, '9.3'
-pod "AFNetworking", "~> 3.0"
+target :TestTabBar do
+
+platform :ios, '9.3'
+pod 'MJExtension', '~> 3.0.10'
+
+end
---
+++ Pods/Target Support Files/Pods-TestTabBar/Pods-TestTabBar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestTabBar : NSObject
+@end
+@implementation PodsDummy_Pods_TestTabBar
+@end
